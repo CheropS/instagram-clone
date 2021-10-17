6e9e8eae4341b8b331c5fa112ddda39d25d85374
Sharon Cherop <sh.cherop@gmail.com>
2021-10-17 08:03:59 +0300

    fixed conf settings to remove.url

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def welcome():
-    return render('/all-insta/home.html')
+def welcome(request):
+    return render(request, '/all-insta/home.html')
