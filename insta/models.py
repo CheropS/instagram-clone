4718fcf007e39cc0e654c1828eb2ab3d85734b3e
Sharon Cherop <sh.cherop@gmail.com>
2021-10-19 15:40:51 +0300

    created tests to see if the model works-fail

--- insta/models.py
+++ insta/models.py
@@ -1,19 +1,60 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime as dt
+from django.utils import timezone
 
 
 # Create your models here.
+
 class Post(models.Model):
-    image=models.ImageField()
-    description=models.TextField()
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
-    created=models.DateTimeField(auto_now_add=True)
-    modified=models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE,related_name='author')
+    image = models.ImageField(blank = True,null=True)
+    caption = models.TextField()
+    created_date = models.DateTimeField(default= timezone.now)
+    updated = models.DateTimeField(auto_now= True)
+    liked = models.ManyToManyField(User, default=None,blank= True,related_name='liked')
+
+    @property
+    def num_likes(self):
+        return self.liked.all().count
+   
+    @classmethod
+    def search_by_title(cls,search_term):
+        author = cls.objects.filter(author__icontains=search_term)
+        return author
+        
+    def __str__(self):
+        return f'{self.author.username}'
 
 class Comment(models.Model):
-    post=models.ForeignKey(Post, on_delete=models.CASCADE)
-    text=models.TextField()
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
-    created=models.DateTimeField(auto_now_add=True)
-    modified=models.DateTimeField(auto_now=True)
+    post = models.ForeignKey('Post',related_name='comments',on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    comment_body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.post.caption,self.name}'
+
+
+LIKE_CHOICES = (
+    ('Like','Like'),
+    ('Unlike','Unlike'),
+)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES,default='Like',max_length=10)
+
+    def __str__(self):
+        return str(self.post)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to ='profile_pics')
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,14 @@
+
 from django.test import TestCase
+from .models import Post, Profile, Like, Comment
 
 # Create your tests here.
+class PostTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.sharry= Profile(user = 'Sharry')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sharry,Profile))
