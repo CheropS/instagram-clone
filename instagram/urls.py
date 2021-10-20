a5ae3390197c349eefba8353840543d70d6e8614
Sharon Cherop <sh.cherop@gmail.com>
2021-10-20 11:25:48 +0300

    adding django registration

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,11 +18,15 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('insta.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    # path('^logout/$', views.logout, {"next_page": '/'})
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
